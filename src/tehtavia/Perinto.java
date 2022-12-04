2020d9a14ab243d1a7fc4c5ea7cb83ae8577b8e6
Topias Liljegren <topias.liljegren@gmail.com>
2022-12-04 10:58:14 +0200

    vaiheet 2 ja 3 valmiit

--- src/tehtavia/Perinto.java
+++ src/tehtavia/Perinto.java
@@ -13,32 +13,36 @@ public class Perinto {
 	private static List<Perija> perijat = new ArrayList<>();
 	
 	public static void main(String[] args) {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String perinto = "";
-		
-	    try {
-			perinto = br.readLine();
-		} catch (IOException ioe) {
-			System.out.println(ioe);
-		}
-	    
-	    String[] numerot = perinto.split(" ");
-	    kuollutID = Integer.parseInt(numerot[0]);
-	    potti = Integer.parseInt(numerot[1]);
-		
-	    while (true) {
-			try {
-				String perija = br.readLine();
-				if (perija.equals("0")) break;
-				String[] osat = perija.split(" ");
-				Perija uusi = new Perija(Integer.parseInt(osat[0]), osat[1], Integer.parseInt(osat[2]), Integer.parseInt(osat[3]));
-				perijat.add(uusi);
+		while (true) {
+		    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		    
+			String perinto = "";
+			
+		    try {
+				perinto = br.readLine();
 			} catch (IOException ioe) {
 				System.out.println(ioe);
 			}
+		    if (perinto == null || perinto.equals("")) break;
+		    
+		    String[] numerot = perinto.split(" ");
+		    kuollutID = Integer.parseInt(numerot[0]);
+		    potti = Integer.parseInt(numerot[1]);
+			
+		    while (true) {
+				try {
+					String perija = br.readLine();
+					if (perija.equals("0")) break;
+					String[] osat = perija.split(" ");
+					Perija uusi = new Perija(Integer.parseInt(osat[0]), osat[1], Integer.parseInt(osat[2]), Integer.parseInt(osat[3]));
+					perijat.add(uusi);
+				} catch (IOException ioe) {
+					System.out.println(ioe);
+				}
+			}
+			
+			laskePerinto();
 		}
-		
-		laskePerinto();
 	}
 	
 	public static void laskePerinto() {
@@ -50,7 +54,7 @@ public class Perinto {
 		Perija kuollut = new Perija(0, "", 0, 0);
 		for (Perija p: perijat) { if (Math.abs(p.id) == kuollutID) kuollut = p;}
 		kuollut.tulot = potti;
-		kuollut.tulot = kuollut.laskePerinto(perijat);
+		kuollut.tulot = kuollut.laskePerinto(perijat, 0);
 		boolean oliko = false;
 		for (Perija p: perijat) {
 			if (p.tulot != 0 && Math.abs(p.id) != kuollutID) {
@@ -85,23 +89,28 @@ class Perija {
 		System.out.println(this.nimi + " saa " + tulot);
 	}
 	
-	public int laskePerinto(List<Perija> kaikki) {
+	public int laskePerinto(List<Perija> kaikki, int iter) {
 		List<Perija> perijat = new ArrayList<>();
 		for (Perija p: kaikki) {
 			if (p.vanhempi1 == Math.abs(this.id) || p.vanhempi2 == Math.abs(this.id))
 				perijat.add(p);
+			if (iter == 0 && (this.vanhempi1 == Math.abs(p.id) || this.vanhempi2 == Math.abs(p.id)))
+				perijat.add(p);
 		}
+		
 		int ekstra = 0;
 		int vahempi = 0;
 		for (Perija p: perijat) {
 			int apu = 0;
-			if (p.id < 0) apu = p.laskePerinto(kaikki);
+			if (p.id < 0) apu = p.laskePerinto(kaikki, iter + 1);
 			if (apu < 0) vahempi++;
 			else ekstra += apu;
 		}
+		
 		for (Perija p: perijat) {
 			if (p.id >= 0) p.tulot = this.tulot / (perijat.size() - vahempi);
 		}
+		
 		int apu = tulot;
 		tulot = 0;
 		if (perijat.size() == 0) return -1;
